7d8dfc9006739b1e1ec17d56f7f8c97792c9f98d
Noman <nomans.protonmail@proton.me>
2025-10-27 16:10:13 +0600

    v1.0

--- dist/plugin.js
+++ dist/plugin.js
@@ -3,5 +3,5 @@ exports.version = 1.0
 exports.description = "Displays info about server, disk and hfs in user panel."
 exports.preview = "https://github.com/8gudbits/hfs-server-info/raw/main/preview.png"
 exports.apiRequired = 1;
-exports.frontend_js = ["main.js"]
-exports.frontend_css = ["styles.css"]
+exports.frontend_js = "main.js"
+exports.frontend_css = "styles.css"
